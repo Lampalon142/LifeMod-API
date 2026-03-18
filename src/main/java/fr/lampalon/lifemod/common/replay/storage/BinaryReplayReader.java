cf866f8bcbade73f00c0bf92fe9397bcf982e737
Lampalon <lampalonpro@gmail.com>
2026-03-18 22:30:42 +0100

    feat: add BinaryReplayReader for file playback

---
+++ src/main/java/fr/lampalon/lifemod/common/replay/storage/BinaryReplayReader.java
@@ -0,0 +1,38 @@
+package fr.lampalon.lifemod.common.replay.storage;
+
+import fr.lampalon.lifemod.common.replay.packet.ReplayFrame;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Reader for binary replay files.
+ */
+public class BinaryReplayReader {
+
+    private final File replayFile;
+
+    public BinaryReplayReader(File replayFile) {
+        this.replayFile = replayFile;
+    }
+
+    /**
+     * Reads all frames from the replay file.
+     * @return List of ReplayFrame objects.
+     */
+    public List<ReplayFrame> readAllFrames() {
+        List<ReplayFrame> frames = new ArrayList<>();
+        try (DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(replayFile)))) {
+            while (inputStream.available() > 0) {
+                long timestamp = inputStream.readLong();
+                int packetCount = inputStream.readInt();
+                
+                // Packet deserialization logic here (requires PacketEvents wrapping)
+                frames.add(new ReplayFrame(timestamp, new ArrayList<>()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return frames;
+    }
+}
