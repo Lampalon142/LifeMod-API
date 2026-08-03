55782cbad4296fd28054bc4bf1a2ead313c91c5d
Lampalon142 <lampalonpro@gmail.com>
2026-08-03 21:06:30 +0200

    fix(noclip): restore spectator traversal with client gamemode spoof

--- platform/bukkit/src/main/java/fr/lampalon/lifemod/platform/bukkit/listeners/NoClipListener.java
+++ platform/bukkit/src/main/java/fr/lampalon/lifemod/platform/bukkit/listeners/NoClipListener.java
@@ -1,10 +1,13 @@
 package fr.lampalon.lifemod.platform.bukkit.listeners;
 
 import fr.lampalon.lifemod.platform.bukkit.managers.NoClipManager;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class NoClipListener implements Listener {
 
@@ -22,5 +25,21 @@ public class NoClipListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         noClipManager.handleJoin(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onGameModeChange(PlayerGameModeChangeEvent event) {
+        if (!noClipManager.isNoClip(event.getPlayer().getUniqueId())) return;
+        GameMode target = event.getNewGameMode();
+        if (target != GameMode.SPECTATOR) {
+            noClipManager.disableNoClip(event.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        if (noClipManager.isNoClip(event.getPlayer().getUniqueId())) {
+            noClipManager.disableNoClip(event.getPlayer());
+        }
     }
 }
